d449df4be79cbda43129a60313a5a2a8024d484f
adamchok <adam.c11304@gmail.com>
2024-08-12 10:00:40 +0800

    Project creation

---
+++ src/main/java/com/project/learn_refesh_jwt/LearnRefeshJwtApplication.java
@@ -0,0 +1,13 @@
+package com.project.learn_refesh_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnRefeshJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnRefeshJwtApplication.class, args);
+	}
+
+}
